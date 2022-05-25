28df10932a28762fa51a18ce198bb06fc9687a15
narminteletoob <narminkt@code.edu.az>
2022-05-25 15:15:30 +0400

    first commit

---
+++ file.js
@@ -0,0 +1,61 @@
+const companies = [
+
+    { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+]
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+//for (let index = 0; index < companies.length; index++) {
+//console.log(companies[i]);
+//}
+//companies.forEach(function(company) {
+//console.log(companyname);
+
+//});
+//filter
+//letcanDrink = [];
+//for (let index = 0; index < ages.length; index++) {
+//if (ages[i] >= 21) {
+//canDrink.push(ages[i]);
+//}
+
+//}
+///const canDrink = ages.filter(function(age) {
+//if (age >= 21) {
+//return true;
+//}
+//}
+//map
+//const companyNames = companies.map(function(company) {
+
+//return company.name;
+
+//})
+//const testMap = companies.map(company = > '${company.name')[$ { company.start } - $ {
+//compan yield.end
+//}]);
+//}]
+//sort
+//const sortedCompanies = companies.sort(function(c1, c2) {
+//if (c1.start) {
+//  return 1;
+// } else {
+//
+//
+//return -1;
+//}
+//});
+//const sortedCompanies = companies.sory((a, b) => (a.start > b.start & 1: -1));
+//console.log(sortedCompanies);
+//reduce
+//let ageSum = ages.reduce((total, age) => total + age, 0);
+//const totalYears = companies.reduce(function(total, company) {
+//  return total +
+//});
+//console.log(ageSum);
